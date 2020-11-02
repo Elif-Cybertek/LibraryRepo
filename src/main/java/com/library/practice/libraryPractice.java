d6aded5bf4de534a281a07af5acafb68bb87c1f5
Elif-Cybertek <68970544+Elif-Cybertek@users.noreply.github.com>
2020-11-02 10:25:34 -0600

    Update libraryPractice.java

--- src/main/java/com/library/practice/libraryPractice.java
+++ src/main/java/com/library/practice/libraryPractice.java
@@ -2,6 +2,6 @@ package com.library.practice;
 
 public class libraryPractice {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello, blue,pink");
     }
 }
